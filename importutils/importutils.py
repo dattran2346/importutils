7f74e059b03dffeb49caffec67d06597b5db0ac4
Tran Quang Dat <dattran2346@gmail.com>
2019-06-24 22:57:24 +0700

    Autoreload and lazy import conflict, cant resolve yet 😢

--- importutils/importutils.py
+++ importutils/importutils.py
@@ -1,36 +1,4 @@
-# Import torch
 import lazy_import as lazy
-
-def lazily(name):
-    return lazy.lazy_module(name)
-
-# lazy import neccessary module
-torch = lazily('torch')
-nn = lazily('torch.nn')
-F = lazily('torch.nn.functional')
-init = lazily('torch.nn.init')
-models = lazily('torchvision.models')
-optim = lazily('torch.optim')
-torchvision = lazily('torchvision')
-# Dataset = lazily('torch.utils.data.Dataset')
-# DataLoader = lazily('torch.utils.data.DataLoader')
-
-plt = lazily('matplotlib.pyplot')  
-np = lazily('numpy')
-pd = lazily('pandas')
-cv2 = lazily('cv2')
-Image = lazily('PIL.Image')
-
-os = lazily('os')
-shutil = lazily('shutil')
-sys = lazily('sys')
-glob = lazily('glob')
-random = lazily('random')
-pickle = lazily('pickle')
-Path = lazily('pathlib.Path')
-datetime = lazily('datetime')
-time = lazily('time')
-
 
 # ipython magic
 def get_env_name():
@@ -43,14 +11,62 @@ def get_env_name():
     except NameError:
         return "Script"
 
-# run default ipython magic
-env = get_env_name()
-if env != "Script":
-    ipython = get_ipython()
-    ipython.magic('reload_ext autoreload')
-    ipython.magic('autoreload 2')
+def lazily_module(name):
+    module = lazy.lazy_module(name)
+    try: 
+        get_ipython().magic('aimport -%s' % name)
+        print('Not autoreload %s' % name)
+    except: pass
+    return module
 
-    if env == 'Jupyter':
-        ipython.magic('matplotlib inline')
-    else:
-        ipython.magic('matplotlib tk')
+def lazily_class(name):
+    module = lazy.lazy_class(name)
+    try: 
+        get_ipython().magic('aimport -%s' % name)
+        print('Not autoreload %s' % name)
+    except: pass
+    return module
+
+# lazy import neccsessary module
+# torch = lazily_module('torch')
+# nn = lazily_module('torch.nn')
+# F = lazily_module('torch.nn.functional')
+# init = lazily_module('torch.nn.init')
+# models = lazily_module('torchvision.models')
+# optim = lazily_module('torch.optim')
+# torchvision = lazily_module('torchvision')
+# Dataset = lazily_class('torch.utils.data.Dataset')
+# DataLoader = lazily_class('torch.utils.data.DataLoader')
+
+plt = lazily_module('matplotlib.pyplot')  
+np = lazily_module('numpy')
+# pd = lazily_module('pandas')
+cv2 = lazily_module('cv2')
+Image = lazily_module('PIL.Image')
+
+# builtin lib > import anyway
+os = lazily_module('os')
+shutil = lazily_module('shutil')
+sys = lazily_module('sys')
+glob = lazily_module('glob')
+random = lazily_module('random')
+pickle = lazily_module('pickle')
+Path = lazily_class('pathlib.Path')
+datetime = lazily_module('datetime')
+time = lazily_module('time')
+
+
+
+# run default ipython magic
+# env = get_env_name()
+# if env != "Script":
+#     ipython = get_ipython()
+#     ipython.magic('reload_ext autoreload')
+#     # ipython.run_cell_magic('reload_ext', 'autoreload')
+#     ipython.magic('autoreload 2')
+#     # ipython.run_cell_magic('autoreload', '2')
+
+#     if env == 'Jupyter':
+#         ipython.magic('matplotlib inline')
+#     else:
+#         ipython.magic('matplotlib tk')
