4a6d82b6ed6750f9ea1d29464ad6e26e523eadde
Tran Quang Dat <dattran2346@gmail.com>
2019-06-24 19:35:19 +0700

    First try lazily import

--- importutils/importutils.py
+++ importutils/importutils.py
@@ -1,19 +1,38 @@
 # Import torch
-try:
-    import torch
-    from torch import nn
-    from torch.nn import init
-    import torch.nn.functional as F
-    from torchvision.models import * # import all model
-    from torch import optim
-    from torch.utils.data import Dataset, DataLoader
-    import torchvision
-    from torchvision import transforms
-    print('import torch and torchvision')
-except:
-    print('torch module not found ')
+import lazy_import as lazy
+
+def lazily(name):
+    return lazy.lazy_module(name)
+
+# lazy import neccessary module
+torch = lazily('torch')
+nn = lazily('torch.nn')
+F = lazily('torch.nn.functional')
+init = lazily('torch.nn.init')
+models = lazily('torchvision.models')
+optim = lazily('torch.optim')
+torchvision = lazily('torchvision')
+# Dataset = lazily('torch.utils.data.Dataset')
+# DataLoader = lazily('torch.utils.data.DataLoader')
+
+plt = lazily('matplotlib.pyplot')  
+np = lazily('numpy')
+pd = lazily('pandas')
+cv2 = lazily('cv2')
+Image = lazily('PIL.Image')
+
+os = lazily('os')
+shutil = lazily('shutil')
+sys = lazily('sys')
+glob = lazily('glob')
+random = lazily('random')
+pickle = lazily('pickle')
+Path = lazily('pathlib.Path')
+datetime = lazily('datetime')
+time = lazily('time')
 
 
+# ipython magic
 def get_env_name():
     try:
         cls = get_ipython().__class__.__name__
@@ -35,50 +54,3 @@ if env != "Script":
         ipython.magic('matplotlib inline')
     else:
         ipython.magic('matplotlib tk')
-
-    
-# Import matplotlib
-try:
-    import matplotlib.pyplot as plt
-    print('import matplotlib.pyplot as plt')
-except:
-    print('matplotlib module not found')
-
-# Import numpy
-try:
-    import numpy as np
-    print('import numpy as np')
-except:
-    print('numpy module not found')
-
-# Import pandas
-try:
-    import pandas as pd
-    print('import pandas as pd')
-except:
-    print('pandas module not found')
-
-# Import cv2
-try:
-    import cv2
-    print('import cv2')
-except:
-    print('cv2 module not found')
-
-try:
-    from PIL import Image
-    print('Import PIL')
-except:
-    print('PIL module not found')
-
-
-# import default python library
-import os
-import sys
-import glob
-import random
-import pickle
-from pathlib import Path
-import datetime
-import time
-import shutil
